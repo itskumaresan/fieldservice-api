a327d3fd086338926f264dc0a85733f8f56544f9
itskumaresan <itskumaresan@users.noreply.github.com>
2019-04-14 10:23:30 +0800

    Project Setup
    Project Setup

---
+++ src/main/java/com/gaksvytech/fieldservice/FieldserviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.gaksvytech.fieldservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldserviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldserviceApiApplication.class, args);
+	}
+
+}
