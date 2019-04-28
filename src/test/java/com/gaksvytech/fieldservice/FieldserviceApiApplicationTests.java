a849135a06d59fa892ac05c7d152c384bf9915cf
itskumaresan <itskumaresan@users.noreply.github.com>
2019-04-28 22:46:11 +0800

    Atleast one test case should be there

--- src/test/java/com/gaksvytech/fieldservice/FieldserviceApiApplicationTests.java
+++ src/test/java/com/gaksvytech/fieldservice/FieldserviceApiApplicationTests.java
@@ -1,18 +1,23 @@
 package com.gaksvytech.fieldservice;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.gaksvytech.fieldservice.scheduler.EventSchedulerEngine;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FieldserviceApiApplicationTests {
 	
-	/*
-	 * @Autowired public EventSchedulerEngine eventSchedulerEngine;
-	 * 
-	 * @Test public void runSchedule() throws Exception {
-	 * eventSchedulerEngine.process(); }
-	 */
+	@Autowired
+	public EventSchedulerEngine eventSchedulerEngine;
+	
+	@Test
+	public void runSchedule() throws Exception {
+		//eventSchedulerEngine.process();
+	}
 
 }
